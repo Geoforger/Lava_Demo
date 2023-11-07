754ef4d81ad3597967978ff566143158676ec7ec
Geoforger <brayshaw.george@gmail.com>
2023-11-07 11:58:02 +0000

    Added and tested 'create_lava_array' method

--- utils/data_processor.py
+++ utils/data_processor.py
@@ -14,6 +14,7 @@ TODO: Refactor messy code throughout
 # ------------------------------------------------------------------------------------------------------------------------------------------------------
 import pickle
 import numpy as np
+
 # import os
 # import nums_from_string     # pip install nums_from_string
 import time
@@ -23,8 +24,9 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 
-class DataProcessor():
-    """ Class to process neuroTac data. Use the .load_data method to load from a previously saved file
+
+class DataProcessor:
+    """Class to process neuroTac data. Use the .load_data method to load from a previously saved file
     Arguments
     ---------
     data:   numpy array of lists
@@ -34,29 +36,28 @@ class DataProcessor():
     """
 
     def __init__(self, data=None, AER=False):
-
         self.data = data
         # Flag to indicate if data has been converted to AER and thus made unavailable for most  operations
         self.__AER = AER
 
     @classmethod
     def load_data(cls, path: str, AER: bool = False):
-        """ Class method to load data in from a file
+        """Class method to load data in from a file
 
         path (string): Path string to the location of data if load_data = True
 
         """
-        with(open(path, "rb")) as openfile:
+        with open(path, "rb") as openfile:
             try:
                 data = pickle.load(openfile)
             except EOFError:
                 print(EOFError)
 
         return cls(data, AER=AER)
-    
+
     @classmethod
     def load_data_np(cls, path: str, AER: bool = False):
-        """ Class method to load data in from a file
+        """Class method to load data in from a file
 
         path (string): Path string to the location of data if load_data = True
 
@@ -66,8 +67,7 @@ class DataProcessor():
         return cls(data, AER=AER)
 
     def data_show(self):
-        """" Function to print the current data stored in the class
-        """
+        """ " Function to print the current data stored in the class"""
         print(self.data)
 
     def plot_data(self, display=False, normalise=False):
@@ -77,8 +77,8 @@ class DataProcessor():
         unproc_data = np.zeros(self.data.shape)
         for y in range(self.data.shape[0]):
             for x in range(self.data.shape[1]):
-                for spike in self.data[y,x]:
-                    unproc_data[y,x] += 1
+                for spike in self.data[y, x]:
+                    unproc_data[y, x] += 1
 
         if normalise:
             ax = sns.heatmap(unproc_data, linewidth=0.0, norm=LogNorm())
@@ -91,10 +91,8 @@ class DataProcessor():
         return ax
 
     def remove_empty(self):
-        """" Function to remove empty lists from input data
-        """
+        """ " Function to remove empty lists from input data"""
         if not self.__AER:
-
             new_list = np.empty_like(self.data)
 
             for row in range(self.data.shape[0]):
@@ -107,28 +105,29 @@ class DataProcessor():
 
         else:
             raise ValueError(
-                'Data is in AER format and cannot perform remove_empty operation')
+                "Data is in AER format and cannot perform remove_empty operation"
+            )
 
     # Function is currently bugged
     def remove_duplicates(self):
-        """ Function to remove duplicate lists from input data
-        """
+        """Function to remove duplicate lists from input data"""
         if not self.__AER:
             new_list = np.empty_like(self.data)
 
             for row in range(self.data.shape[0]):
                 for column in range(self.data.shape[1]):
                     # Use list(set()) to remove duplicates in list (sets cannot contain dupes)
-                    new_list[row, column] = (list(set(self.data[row, column])))
+                    new_list[row, column] = list(set(self.data[row, column]))
 
             self.data = new_list
 
         else:
             raise ValueError(
-                'Data is in AER format and cannot perform remove_duplicates operation')
+                "Data is in AER format and cannot perform remove_duplicates operation"
+            )
 
     def remove_cuttoff(self, cuttoff, remove_dup=False, remove_em=False):
-        """ Function to remove events after a specified cuttoff point
+        """Function to remove events after a specified cuttoff point
         Arguments
         ---------
         cuttoff:        int
@@ -136,7 +135,7 @@ class DataProcessor():
         remove_dup:     Bool (default=False)
                             Bool to indicate if you wish to remove duplicate spikes before processing
         remove_em:      Bool (default=False)
-                            Bool to indicate if you wish to remove empty pixels before processing                        
+                            Bool to indicate if you wish to remove empty pixels before processing
         """
         if not self.__AER:
             if remove_dup:
@@ -150,16 +149,20 @@ class DataProcessor():
                 for column in range(self.data.shape[1]):
                     # Create new list for each pixel, only containing values lower than cuttoff
                     new_list[row, column] = [
-                        element for element in self.data[row, column] if element <= cuttoff]
+                        element
+                        for element in self.data[row, column]
+                        if element <= cuttoff
+                    ]
 
             self.data = new_list
 
         else:
             raise ValueError(
-                'Data is in AER format and cannot perform remove_cuttoff operation')
+                "Data is in AER format and cannot perform remove_cuttoff operation"
+            )
 
     def pixel_reduction(self, x_reduce_l, x_reduce_r, y_reduce_t, y_reduce_b):
-        """ Function to reduce the number of pixels in output image from the neuroTac
+        """Function to reduce the number of pixels in output image from the neuroTac
 
         Arguments
         ----------
@@ -192,7 +195,8 @@ class DataProcessor():
 
         else:
             raise ValueError(
-                'Data is in AER format and cannot perform pixel_reduction operation')
+                "Data is in AER format and cannot perform pixel_reduction operation"
+            )
 
     def __bitstring_to_bytes(self, s):
         """
@@ -201,12 +205,12 @@ class DataProcessor():
         v = int(s, 2)
         b = bytearray()
         while v:
-            b.append(v & 0xff)
+            b.append(v & 0xFF)
             v >>= 8
         return bytes(b[::-1])
 
     def convert_to_aer(self, ON_OFF=1):
-        """ Function to create convert pickled neuroTac data into aer format .bin file
+        """Function to create convert pickled neuroTac data into aer format .bin file
 
         Arguments
         ----------
@@ -237,21 +241,20 @@ class DataProcessor():
 
         # Convert to 40 bit binary number
         for element in sorted_list:
-
             # Add each of x,y,p & ts of the list to the byte array
-            #print(f'element[0] = {element[0]}')
+            # print(f'element[0] = {element[0]}')
             if element[0] == 0:
                 string.extend(bytearray([0]))
             else:
                 string.extend(bytearray([element[0]]))
 
-            #print(f'element[1] = {element[1]}')
+            # print(f'element[1] = {element[1]}')
             if element[1] == 0:
                 string.extend(bytearray([0]))
             else:
                 string.extend(bytearray([element[1]]))
 
-            #print(f'element[2] = {element[2]}')
+            # print(f'element[2] = {element[2]}')
             if element[2] == 0:
                 string.extend(bytearray([0]))
             else:
@@ -264,8 +267,7 @@ class DataProcessor():
                 string.extend(bytearray([element[3]]))
             # If it's less than 2 bytes add 1 bytes padding
             elif 256 <= element[3] <= 65536:
-                string.extend(self.__bitstring_to_bytes(
-                    '{0:016b}'.format(element[3])))
+                string.extend(self.__bitstring_to_bytes("{0:016b}".format(element[3])))
             # If it's larger than 2 bytes, no padding required
 
             # Function can only handle ts of under 65536 currently
@@ -279,27 +281,27 @@ class DataProcessor():
         self.data = string
 
     def save_data(self, PATH):
-        """ Function to save processed data
+        """Function to save processed data
         Arguments
         ---------
         PATH:       string
-                        Path to save location for data. NOTE: Currently you must specify the filename and type in this PATH string                      
+                        Path to save location for data. NOTE: Currently you must specify the filename and type in this PATH string
         """
-        with open(PATH, 'wb') as pickle_out:
+        with open(PATH, "wb") as pickle_out:
             pickle.dump(self.data, pickle_out)
             pickle_out.close()
-            
+
     def save_data_np(self, PATH):
         np.save(PATH, self.data, allow_pickle=True)
 
     def offset_values(self, offset, reduce=False):
-        """ Function to offset input data to avoid negative spike times
+        """Function to offset input data to avoid negative spike times
         Arguments
         ---------
         offset:     int
-                        offset to add to each event in ms    
+                        offset to add to each event in ms
         reduce:     bool
-                        Set to true if data is already offset and should be clipped back rather than forwards. Default = False                  
+                        Set to true if data is already offset and should be clipped back rather than forwards. Default = False
         """
         data_y, data_x = self.data.shape
 
@@ -311,17 +313,19 @@ class DataProcessor():
                 if self.data[y, x] != []:
                     # If need to clip data back then remove all before offset and reduce all values by offset
                     if reduce:
-                        temp_list = [(spike - offset)
-                                     for spike in self.data[y, x] if spike >= offset]
+                        temp_list = [
+                            (spike - offset)
+                            for spike in self.data[y, x]
+                            if spike >= offset
+                        ]
                     # Else simply add the offset to each spike
                     else:
-                        temp_list = [(spike + offset)
-                                     for spike in self.data[y, x]]
+                        temp_list = [(spike + offset) for spike in self.data[y, x]]
 
                 self.data[y, x] = temp_list
 
     def create_events(self, ON_OFF=1):
-        """ Function to convert data to a lava SLAYER compatible Event object
+        """Function to convert data to a lava SLAYER compatible Event object
         Arguments
         ---------
         ON_OFF:     int (default = 1)
@@ -333,8 +337,8 @@ class DataProcessor():
             temp_list = []
 
             # Debug
-            #print(f"Maximum number of rows (y max) = {self.data.shape[0]}")
-            #print(f"Maximum number of columns (x max) = {self.data.shape[1]}")
+            # print(f"Maximum number of rows (y max) = {self.data.shape[0]}")
+            # print(f"Maximum number of columns (x max) = {self.data.shape[1]}")
 
             # Cycle through each nested list and check if empty
             # Check row
@@ -349,8 +353,7 @@ class DataProcessor():
                             temp_list.append([x, y, ON_OFF, spike])
 
             # Order by timestamp with earliest spike first
-            sorted_list = sorted(
-                temp_list, key=lambda x: int(x[3]), reverse=False)
+            sorted_list = sorted(temp_list, key=lambda x: int(x[3]), reverse=False)
 
             # Move sorted list elements into arrays
             x_array = []
@@ -367,21 +370,18 @@ class DataProcessor():
 
             # Combine arrays into Event object
             # CHWT format
-            td_event = slayer.io.Event(x_array, y_array, channel_array, ts_array, 1000)
-            
-            # if x_array != []:
-            #     td_event = slayer.io.Event(x_array, y_array, channel_array, ts_array, 1000)
-            # else:
-            #     td_event = slayer.io.Event()
-                
+            td_event = slayer.io.Event(
+                x_event=x_array, y_event=y_array, c_event=channel_array, t_event=ts_array, payload=None)
+
             self.data = td_event
 
             return td_event
 
         else:
             raise ValueError(
-                'Data is in AER format and cannot perform create_tensor operation')
-    
+                "Data is in AER format and cannot perform create_events operation"
+            )
+
     def threshold_pooling(self, kernel_size, stride, threshold):
         """
         Method to apply threshold spiking pooling to the data. Returns the data as a resized (x,y) array.
@@ -402,12 +402,12 @@ class DataProcessor():
         x_remain = (chips_x[-1] + kernel_size[1]) % self.data.shape[1]
 
         # Create a number of rows/columns (containing empty lists) equal to the remainder
-        if y_remain != 0:     
+        if y_remain != 0:
             row = np.empty(self.data.shape[1], dtype=object)
             l = []
             for y in range(self.data.shape[1]):
                 row[y] = l
-            
+
             for _ in range(y_remain):
                 self.data = np.vstack([self.data, row])
 
@@ -416,7 +416,7 @@ class DataProcessor():
             l = []
             for x in range(self.data.shape[0]):
                 col[x] = l
-                
+
             for _ in range(x_remain):
                 self.data = np.c_[self.data, col]
 
@@ -428,8 +428,12 @@ class DataProcessor():
             data_out[chip] = self.__find_square(chips[chip], kernel_size, threshold)
 
         # Reshape vector to 2d
-        pooled_dim_y = self.__calculate_pooling_dim(self.data.shape[0], kernel_size[0], stride, 1)
-        pooled_dim_x = self.__calculate_pooling_dim(self.data.shape[1], kernel_size[1], stride, 1)
+        pooled_dim_y = self.__calculate_pooling_dim(
+            self.data.shape[0], kernel_size[0], stride, 1
+        )
+        pooled_dim_x = self.__calculate_pooling_dim(
+            self.data.shape[1], kernel_size[1], stride, 1
+        )
         self.data = np.reshape(data_out, (pooled_dim_y, pooled_dim_x))
 
         return self.data
@@ -437,7 +441,7 @@ class DataProcessor():
     def __find_chips(self, stride):
         """
         Private method used in pooling operation to find starting locations for each chip
-        
+
         Arguments
         ----------
 
@@ -455,7 +459,7 @@ class DataProcessor():
         x = np.tile(x, y_len)
 
         return y, x, y_len * x_len
-    
+
     def __find_square(self, start, kernel_size, threshold):
         """
         Private method to create a square around a starting position. With the starting position at the top left of the square
@@ -470,11 +474,11 @@ class DataProcessor():
         """
         # Find all elements in square
         start_y, start_x = start
-        kernel_y = np.arange(start_y, start_y+kernel_size[0])
-        kernel_x = np.arange(start_x, start_x+kernel_size[1])
+        kernel_y = np.arange(start_y, start_y + kernel_size[0])
+        kernel_x = np.arange(start_x, start_x + kernel_size[1])
         kernel_y = np.repeat(kernel_y, kernel_size[0])
         kernel_x = np.tile(kernel_x, kernel_size[1])
-        
+
         # Grab all data from kernel
         kernel = self.data[kernel_y, kernel_x]
 
@@ -489,7 +493,6 @@ class DataProcessor():
         timings = vals[counts >= threshold]
 
         return timings
-        
 
     def __calculate_pooling_dim(self, input_dim, kernel_dim, stride, order):
         """
@@ -504,25 +507,26 @@ class DataProcessor():
         """
         return int(((input_dim - kernel_dim) / stride) + (1 * order))
 
-
     def create_lava_array(self, sample_length):
         """
         Method to create a tensor of events compatible with lava input processes
-        
+
         Arguments
         ----------
         sample_length (int): Length of the sample. Used to determine 3rd dimension of output tensor
         """
-        y_size, x_size = np.shape(self.data)
+        y_size, x_size = self.data.shape
         events = self.create_events()
-        
-        event_tensor = events.fill_tensor(torch.zeros(1, y_size, x_size, sample_length, requires_grad=False))
-        
+
+        event_tensor = events.fill_tensor(
+            torch.zeros(1, y_size, x_size, sample_length, requires_grad=False)
+        )
+
         return event_tensor.reshape(-1, sample_length)
+
 
 # Testing of the class
 if __name__ == "__main__":
-
     path_to_data = "/home/farscope2/Documents/PhD/First_Year_Project/SpikingNetsTexture/datasets/TacTip_NM/ntac_2.5_11texture_100trial_slide_test_06101340/Artificial Dataset 9Texture No. 10.pickle"
     OUTPUT_PATH = "/home/farscope2/Documents/PhD/Tactile_Lava/utils/test.bin"
 
@@ -530,7 +534,8 @@ if __name__ == "__main__":
     proc = DataProcessor.load_data(path=path_to_data)
 
     import time
+
     start = time.time()
-    proc.threshold_pooling(kernel_size=(2,2), stride=2, threshold=2)
+    proc.threshold_pooling(kernel_size=(2, 2), stride=2, threshold=2)
     end = time.time()
     print(f"Time taken = {end-start}")
