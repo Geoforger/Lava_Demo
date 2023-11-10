b7b387136c4c57dac8157b719d2e75d5ebb87134
bw14452 <bw14452@bristol.ac.uk>
2023-11-10 11:44:55 +0000

    First draft of live plot viewer - using dynamic update class and matplotlib

--- components/online_visualiser.py
+++ components/online_visualiser.py
@@ -14,7 +14,51 @@ from lava.magma.core.process.variable import Var
 
 from IPython.display import display, clear_output
 import matplotlib.pyplot as plt
+import time
 
+
+plt.ion()
+
+class DynamicUpdate():
+    #Suppose we know the x range
+    min_x = 0
+    max_x = 240
+    min_y = 0
+    max_y = 180
+
+    def on_launch(self):
+        #Set up plot
+        self.figure, self.ax = plt.subplots()
+        self.lines, = self.ax.plot([],[], '.')
+        #Autoscale on unknown axis and known lims on the other
+        # self.ax.set_autoscaley_on(True)
+        self.ax.set_xlim(self.min_x, self.max_x)
+        self.ax.set_ylim(self.min_y, self.max_y)
+        #Other stuff
+        # self.ax.grid()
+
+    def on_running(self, xdata, ydata):
+        #Update data (with the new _and_ the old points)
+        self.lines.set_xdata(xdata)
+        self.lines.set_ydata(ydata)
+        # #Need both of these in order to rescale
+        # self.ax.relim()
+        # self.ax.autoscale_view()
+        #We need to draw *and* flush
+        self.figure.canvas.draw()
+        self.figure.canvas.flush_events()
+
+    #Example
+    def __call__(self):
+        self.on_launch()
+        xdata = []
+        ydata = []
+        for x in np.arange(0,10,0.5):
+            xdata.append(x)
+            ydata.append(np.exp(-x**2)+10*np.exp(-(x-7)**2))
+            self.on_running(xdata, ydata)
+            time.sleep(0.01)
+        return xdata, ydata
 
 class OnlineVisualiser(AbstractProcess):
     """Visualiser process for spike input or output.
@@ -35,7 +79,7 @@ class OnlineVisualiser(AbstractProcess):
         self.proc_params['in_shape'] = in_shape
         self.proc_params['sample_length'] = sample_length
         self.proc_params['frame_shape'] = frame_shape
-        # self.frame_shape = frame_shape
+
         self.data = Var(shape=in_shape, init=np.zeros(in_shape, dtype=int))
         self.idx = Var(shape=in_shape, init=np.zeros(in_shape, dtype=int))
 
@@ -52,11 +96,11 @@ class OnlineVisualiserModel(PyLoihiProcessModel):
     
     def __init__(self, proc_params=None) -> None:
         super().__init__()
-        self.fig = plt.figure(figsize=(24,18))
-        self.ax1 = self.fig.add_subplot() #1, 1, 1
+
+        self.dynamic_figure = DynamicUpdate()
+        self.dynamic_figure.on_launch()
         
         self.sample_length = proc_params["sample_length"]
-        # self.window_size = proc_params["window_size"]
         in_shape = proc_params["in_shape"]
         self.frame_shape = proc_params["frame_shape"]
         
@@ -67,31 +111,12 @@ class OnlineVisualiserModel(PyLoihiProcessModel):
 
     def run_spk(self):
         # Recieve a vector of 0,1s at input
+
         data, idx = self.a_in.recv()
-        idx = np.unravel_index(idx,self.frame_shape[::-1])[::-1]
 
-        # self.data = np.pad(data,
-        #                    pad_width=(0, self.in_port.shape[0] - data.shape[0]))
-        # self.idx = np.pad(idx,
-        #                   pad_width=(0, self.in_port.shape[0] - data.shape[0]))
-        
-        # Add the vector to the array
-        # self.data[:,(self.time_step-1)] = data_in
-        # points = np.nonzero(self.data)
-        
-        # Clear plot and replot data
-        self.ax1.clear()
-        self.ax1.scatter(idx[1], idx[0])
-        
-        # if self.time_step < self.window_size:
-        #     self.ax1.set_xlim(0, self.window_size)
-        # else:
-        #     self.ax1.set_xlim(self.time_step-self.window_size, self.time_step + self.window_size)
-            
-        # self.ax1.set_yticks(np.arange(self.a_in.shape[0]))
-        self.ax1.set_ylabel("y")
-        self.ax1.set_xlabel("x")
+        if len(data)>10:
+            idx = np.unravel_index(idx,self.frame_shape[::-1])[::-1]     
+            self.dynamic_figure.on_running(idx[0],idx[1])
+        # time.sleep(0.001)
 
-        # Live display
-        clear_output(wait=True)
-        display(self.fig)
+
